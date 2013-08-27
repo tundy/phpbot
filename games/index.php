331fc5ca1d396df622e1fbbfabb6ed65f318d743
small-killer <small-killer@centrum.sk>
2013-08-27 10:20:15 +0200

    wrong vars name
    oops

--- games/index.php
+++ games/index.php
@@ -1,10 +1,10 @@
 ﻿<?php
 
-$status = out('getstatus\n');					// Ask for server status
-$data = explode("\n", $status);					// split answer, datas and players
-$cvars = explode('//', $data[1]);				// explode cvars
-$key = array_search('g_modversion', $cvar);		// search for mod version
-$ver = explode('.', $key+1);					// explode version
-require_once($ver[1].'.'.$ver[2].'/'.$grep[1]);	// include version cons
+$status = out('getstatus\n');							// Ask for server status
+$data = explode("\n", $status);							// split answer, datas and players
+$cvars = explode('//', $data[1]);						// explode cvars
+$key = array_search('g_modversion', $cvars);			// search for mod version
+$ver = explode('.', $cvars[$key+1]);					// explode version
+require_once($ver[1].'.'.$ver[2].'/'.$cvars[$key+1]);	// include version cons
 
 ?>
