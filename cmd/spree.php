c7617d904d5b96df972cee8493661b7f7825e962
small-killer <small-killer@centrum.sk>
2013-08-28 10:45:14 +0200

    wrong vars names in spree function
    also added dead spree

--- cmd/spree.php
+++ cmd/spree.php
@@ -8,6 +8,14 @@ if ( !file_exists($file) )
 require_once($file);
 unset($file);
 
+function higest_spree ($killer, $target)
+{
+	if ($players[$killer]->spree->kill->last > $players[$killer]->spree->kill->high)
+		$players[$killer]->spree->kill->high = $players[$killer]->spree->kill->last;
+	if ($players[$target]->spree->dead->last > $players[$target]->spree->dead->high)
+		$players[$target]->spree->dead->high = $players[$target]->spree->dead->last;
+}
+
 function spree ($time, $args)
 {
 	global $spree_start, $spree_tk;
@@ -27,29 +35,39 @@ function spree ($time, $args)
 		// Self Kill
 		if($killer == $target)
 		{
-			if ( $players[$target]->spree >= $spree_start)
+			if ( $players[$target]->spree->kill->last >= $spree_start)
 				say($players[$target]->info["name"]."^3 stopped his/her killing spree.");
-			$players[$target]->spree = 0;
+			$players[$target]->spree->dead->last++;
+			higest_spree($killer, $target);
+			$players[$killer]->spree->kill->last = 0;
 		}
 		// Normal Kill
 		elseif ($players[$killer]->info["team"] == 0 or $players[$killer]->info["team"] != $players[$target]->info["team"])
 		{
-			$players[$killer]->spree++;
+			$players[$target]->spree->dead->last++;
+			$players[$killer]->spree->kill->last++;
+			higest_spree($killer, $target);
 		}
 		// TeamKill
-		{
+		else{
 			switch($spree_tk):
-				case 1:		$players[$killer]->spree++;
+				case 1:		$players[$killer]->spree->kill->last++;
+							$players[$target]->spree->dead->last++;
+							higest_spree($killer, $target);
 							break;
-				case 2:		if ( $players[$killer]->spree > 0)
+				case 2:		if ( $players[$killer]->spree->kill->last > 0)
 							{
-								$players[$killer]->spree--;
+								$players[$killer]->spree->kill->last--;
+								$players[$target]->spree->dead->last++;
+								higest_spree($killer, $target);
 								say($players[$killer]->info["name"]."^3 lower his/her killing spree after teamkill.");
 							}
 							break;
-				case 3:		if ( $players[$killer]->spree > 0)
+				case 3:		if ( $players[$killer]->spree->kill->last > 0)
 							{
-								$players[$killer]->spree = 0;
+								$players[$killer]->spree->kill->last = 0;
+								$players[$target]->spree->dead->last++;
+								higest_spree($killer, $target);
 								say($players[$killer]->info["name"]."^3 reset his/her killing spree after teamkill.");
 							}
 							break;
@@ -57,11 +75,11 @@ function spree ($time, $args)
 			endswitch;
 		}
 		
-		if ($players[$killer]->spree >= $spree_start)
-			say($players[$killer]->info["name"]."^3 is on killing spree. ^7".$players[$killer]->spree."^3 kills in the row.");	
-		if ( $players[$target]->spree >= $spree_start)
+		if ($players[$killer]->spree->kill->last >= $spree_start)
+			say($players[$killer]->info["name"]."^3 is on killing spree. ^7".$players[$killer]->spree->kill->last."^3 kills in the row.");	
+		if ( $players[$target]->spree->kill->last >= $spree_start)
 			say($players[$killer]->info["name"]."^3 stopped ".$players[$target]->info["name"]."^3's killing spree.");
-		$players[$target]->spree = 0;
+		$players[$target]->spree->kill->last = 0;
 	}
 }
 
