10444ba2d42818073ec0cb55d9766d8a092aa978
small-killer <small-killer@centrum.sk>
2013-08-28 10:43:53 +0200

    Fixed version checker
    wrong path and counter slash

--- games/index.php
+++ games/index.php
@@ -1,10 +1,10 @@
 <?php
 
-$status = out("getstatus\n");							// Ask for server status
-$data = explode("\n", $status);							// split answer, datas and players
-$cvars = explode('//', $data[1]);						// explode cvars
-$key = array_search('g_modversion', $cvars);			// search for mod version
-$ver = explode('.', $cvars[$key+1]);					// explode version
-require_once($ver[1].'.'.$ver[2].'/'.$cvars[$key+1]);	// include version cons
+$status = out("getstatus\n");											// Ask for server status
+$data = explode("\n", $status);											// split answer, datas and players
+$cvars = explode('\\', $data[1]);										// explode cvars
+$key = array_search('g_modversion', $cvars);							// search for mod version
+$ver = explode('.', $cvars[$key+1]);									// explode version
+require_once('games/'.$ver[0].'.'.$ver[1].'/'.$cvars[$key+1].'.php');	// include version cons
 
 ?>
