ff165d9563eafa24622b9a0a6242999304bd481f
small-killer <small-killer@centrum.sk>
2013-09-01 22:52:13 +0200

    You have to be spectator for using !! command
    You have to be spectator for using !!  chat command.
    Also added colon after player name.

--- cmd/chat.php
+++ cmd/chat.php
@@ -2,14 +2,16 @@
 
 function cmd_chat ($id, $args)
 {
-  global $players;
+	global $players;
   
-  if( isset($args) )
-  {
-    $msg = implode(" ", $args);
-    
-    say($players[$id].$msg);
-  }
+	if( isset($args) )
+	{
+		if($players[$killer]->info["team"] == TEAM_SPEC)	// You have to be spectator
+		{
+			$msg = implode(" ", $args);
+			say($players[$id]."^3: ".$msg);
+		}
+	}
 }
 
 ?>
