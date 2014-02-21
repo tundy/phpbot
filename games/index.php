42d14a9072774f2e108a21195c0184b5ba39f10c
small-killer <small-killer@centrum.sk>
2014-02-21 23:38:54 +0100

    Better way for searching for Game Version

--- games/index.php
+++ games/index.php
@@ -1,10 +1,7 @@
 <?php
 
-$status = out("getstatus\n");											// Ask for server status
-$data = explode("\n", $status);											// split answer, datas and players
-$cvars = explode('\\', $data[0]);										// explode cvars
-$key = array_search('g_modversion', $cvars);							// search for mod version
-$ver = explode('.', $cvars[$key+1]);									// explode version
-require_once('games/'.$ver[0].'.'.$ver[1].'/'.$cvars[$key+1].'.php');	// include version cons
+$g_modversion = get_cvar("g_modversion");								// get urt version
+$ver = explode('.', $g_modversion);										// explode version
+require_once('games/'.$ver[0].'.'.$ver[1].'/'.$g_modversion.'.php');	// include version cons
 
 ?>
