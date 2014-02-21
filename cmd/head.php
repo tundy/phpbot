c3c3da1bcb0899688bc59953b0203df98e590c12
small-killer <small-killer@centrum.sk>
2014-02-21 23:42:27 +0100

    hs will use tell instead of say if player is spec
    Headshots command will now use Tell function instead of Say Function if
    player is Spectator.

--- cmd/head.php
+++ cmd/head.php
@@ -30,6 +30,15 @@ function say_hs($id) {
 		say($name_color.$players[$id]->info["name"].$text_color." made ".$alt_color.$players[$id]->headshots.$text_color." headshots");
 }
 
+function tell_hs($id, $hs_id) {
+	global $players, $name_color, $text_color, $alt_color;
+	
+	if ( $players[$hs_id]->headshots == 1 )
+		tell($id, $name_color.$players[$hs_id]->info["name"].$text_color." made ".$alt_color.$players[$hs_id]->headshots.$text_color." headshot");
+	else
+		tell($id, $name_color.$players[$hs_id]->info["name"].$text_color." made ".$alt_color.$players[$hs_id]->headshots.$text_color." headshots");
+}
+
 function write_hs($id) {
 	global $players, $name_color, $text_color, $alt_color;
 	
@@ -41,28 +50,51 @@ function write_hs($id) {
 
 function cmd_hs ($id, $args = null) {
 	global $players;
-	
+	debug("\t\tcmd_hs()");
+			
 	if( isset($args[0]) ) {
-		if(preg_match("/[0-9]+/", $args[0], $id)) {		// If Number
-			$id = $id[0];
-			if( isset($players[$id]) )
-				say_hs($id);
+		debug("\t\t\tARG is set.");
+		if(preg_match("/[0-9]+/", $args[0], $hs_id)) {		// If Number
+			$hs_id = $hs_id[0];
+			if( isset($players[$hs_id]) )
+				if($players[$id]->info["team"] == TEAM_SPEC) {
+					debug("\t\t\tPLAYER is spec.");
+					tell_hs($id, $hs_id);
+				} else {
+					debug("\t\t\tPLAYER is not spec.");
+					say_hs($hs_id);
+				}
 			else {										// If ID not found check if it's a name
-				unset($id);
-				$id = search($args[0]);
-				if( isset($id) )
-					say_hs($id);	
+				$hs_id = search($args[0], 0, 0, $id);
+				if( isset($hs_id) )
+					if($players[$id]->info["team"] == TEAM_SPEC) {
+						debug("\t\t\tPLAYER is spec.");
+						tell_hs($id, $hs_id);
+					} else {
+						debug("\t\t\tPLAYER is not spec.");
+						say_hs($hs_id);	
+					}
 			}
+		} else {
+			$hs_id = search($args[0], 0, 0, $id);
+			if( isset($hs_id) )
+				if($players[$id]->info["team"] == TEAM_SPEC) {
+					debug("\t\t\tPLAYER is spec.");
+					tell_hs($id, $hs_id);
+				} else {
+					debug("\t\t\tPLAYER is not spec.");
+					say_hs($hs_id);	
+				}
 		}
-		else {
-			unset($id);
-			$id = search($args[0]);
-			if( isset($id) )
-				say_hs($id);
-		}
-	}
-	else
+	} elseif($players[$id]->info["team"] == TEAM_SPEC) {
+		debug("\t\t\tARG is not set.");
+		debug("\t\t\tPLAYER is spec.");
+		tell_hs($id, $id);
+	} else {
+		debug("\t\t\tARG is not set.");
+		debug("\t\t\tPLAYER is not spec.");
 		say_hs($id);
+	}
 }
 
 ?>
