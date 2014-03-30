2712550a120da62cfca3e64e7c4c2e151a3a083c
small-killer <small-killer@centrum.sk>
2014-03-30 16:01:16 +0200

    Update index.php
    Debug

--- games/index.php
+++ games/index.php
@@ -2,6 +2,7 @@
 
 $g_modversion = get_cvar("g_modversion");								// get urt version
 $ver = explode('.', $g_modversion);										// explode version
+debug("Version: $g_modversion", 2);
 require_once('games/'.$ver[0].'.'.$ver[1].'/'.$g_modversion.'.php');	// include version cons
 
 ?>
