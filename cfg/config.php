176b864d8b312b1f9af8bcd55557e751865bd4e6
Tundy Matúš Tundér <tunder.matus@gmail.com>
2016-12-31 19:30:47 +0100

    changed path

--- cfg/config.php
+++ cfg/config.php
@@ -4,7 +4,8 @@ $port			= 27960;
 $say_prefix		= '';
 $tell_prefix	= '';
 $rcon			= 'rconpw';
-$log			= '/home/urt/UrbanTerror42/q3ut4/games.log';
+//$log			= '/home/urt/UrbanTerror42/q3ut4/games.log';
+$log			= 'E:\\Games\\UrbanTerror 4.3\\q3ut4\\games.log';
 $logfile		= 'bot.log';
 
 $plugins[]		= 'plugins/head.php';
